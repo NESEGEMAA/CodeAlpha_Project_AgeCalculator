2b578cf77fde11ffb055f1e6b256278f0cf4b6a6
nesegemaa <Xxnour998xX@gmail.com>
2024-04-28 22:22:33 +0200

    changed app,js to app.js (Typo)

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("dateForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the value of the date input field
+    var dateInput = document.getElementById("birthdate").value;
+
+    // Display the value
+    var output = document.getElementById("output");
+    output.innerHTMl = "Selected date: " + dateInput;
+});
